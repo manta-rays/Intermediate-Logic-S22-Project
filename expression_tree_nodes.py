d1205588f39e4da9b8f524aba43c774e9acc61f5
manta-rays <90805992+manta-rays@users.noreply.github.com>
2022-04-23 15:42:17 -0400

    asdf
    asdf

---
+++ expression_tree_nodes.py
@@ -0,0 +1,47 @@
+""" Operator nodes """
+
+class IFF(object):
+	def __init__(self, left, right):
+		self.left = left
+		self.right = right
+
+	def evaluate(self, context):
+		return self.left.evaluate(context) == self.right.evaluate(context)
+
+class AND(object):
+	def __init__(self, left, right):
+		self.left = left
+		self.right = right
+
+	def evaluate(self, context):
+		return self.left.evaluate(context) and self.right.evaluate(context)
+
+class OR(object):
+	def __init__(self, left, right):
+		self.left = left
+		self.right = right
+
+	def evaluate(self, context):
+		return self.left.evaluate(context) or self.right.evaluate(context)
+
+class NOT(object):
+	def __init__(self, left):
+		self.left = left
+
+	def evaluate(self, context):
+		return not self.left.evaluate(context)
+
+class THEN(object):
+	def __init__(self, left, right):
+		self.antecedent = antecedent
+		self.consequent = consequent
+
+	def evaluate(self, context):
+		return (not self.left.evaluate(antecedent)) or self.right.evaluate(consequent)
+
+class VAR(object):
+	def __init__(self, name):
+		self.name = name
+
+	def evaluate(self, context):
+		return context[self.name]
